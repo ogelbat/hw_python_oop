4081f4eb525add04b2e864f14eeec47d1b066b94
Oleg Volkov <ogelbat@yandex.ru>
2021-11-08 21:37:32 +0300

    Complated Home Work ver 1.03.
    Объявил класы через декоратор @dataclass
    Добавлены исключения при проверке пакетов:
     ValueError('Ошибка размера пакета данных')
     TypeError('Неизвестный тип тренировки')
    
    # 1я Попытка победить PEP8

--- homework.py
+++ homework.py
@@ -12,11 +12,11 @@ class InfoMessage:
     calories: float  # Потрачено ккал
 
     def get_message(self) -> str:
-        temp = (f'Тип тренировки: {self.training_type};' +
-                f' Длительность: {self.duration:.3f} ч.;' +
-                f' Дистанция: {self.distance:.3f} км;' +
-                f' Ср. скорость: {self.speed:.3f} км/ч;' +
-                f' Потрачено ккал: {self.calories:.3f}.')
+        temp = (f'Тип тренировки: {self.training_type};'
+                + f' Длительность: {self.duration:.3f} ч.;'
+                + f' Дистанция: {self.distance:.3f} км;'
+                + f' Ср. скорость: {self.speed:.3f} км/ч;'
+                + f' Потрачено ккал: {self.calories:.3f}.')
         return temp
 
 
