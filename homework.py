94087e17b211e27fb03916bfe6b342accf5be1a1
Oleg Volkov <ogelbat@yandex.ru>
2021-11-10 18:16:57 +0300

    Complated Home Work ver 1.04.
    Переписал математическю часть функций (убрал переменные temp)
    
    Переписал логику вызова исключений при поступлении не правильных пакетов

--- homework.py
+++ homework.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from typing import Union
+from typing import List
+from typing import ClassVar
 
 
 @dataclass
@@ -12,12 +15,11 @@ class InfoMessage:
     calories: float  # Потрачено ккал
 
     def get_message(self) -> str:
-        temp = (f'Тип тренировки: {self.training_type};'
-                + f' Длительность: {self.duration:.3f} ч.;'
-                + f' Дистанция: {self.distance:.3f} км;'
-                + f' Ср. скорость: {self.speed:.3f} км/ч;'
-                + f' Потрачено ккал: {self.calories:.3f}.')
-        return temp
+        return (f'Тип тренировки: {self.training_type};'
+                f' Длительность: {self.duration:.3f} ч.;'
+                f' Дистанция: {self.distance:.3f} км;'
+                f' Ср. скорость: {self.speed:.3f} км/ч;'
+                f' Потрачено ккал: {self.calories:.3f}.')
 
 
 @dataclass
@@ -28,18 +30,14 @@ class Training:
     duration: float  # длительность тренировки.
     weight: float  # вес спортсмена.
 
-    LEN_STEP = 0.65  # расстояние, которое преодалевает спортсмен.
-    M_IN_KM = 1000  # константа перевода метров в километры.
-    coeff_calorie_1 = 18  # Очень хочеться сделать все атрибуты
-    coeff_calorie_2 = 20  # приватными, но pytest не разрешает :(
-    coeff_calorie_3 = 0.035
-    coeff_calorie_4 = 0.029
-    coeff_calorie_6 = 1.1
-    coeff_calorie_7 = 2
+    LEN_STEP: ClassVar[float] = 0.65  # расстояние одного шага
+    M_IN_KM: ClassVar[int] = 1000  # константа перевода метров в километры.
+    coeff_calorie_1: ClassVar[Union[int, float]] = 18  # Коэф-т для расчета
+    coeff_calorie_2: ClassVar[Union[int, float]] = 20  # Коэф-т для расчета
 
     def get_distance(self) -> float:
         """Получить дистанцию в км."""
-        return (self.action * self.LEN_STEP) / self.M_IN_KM
+        return self.action * self.LEN_STEP / self.M_IN_KM
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
@@ -64,9 +62,12 @@ class Running(Training):
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        temp = self.coeff_calorie_1 * self.get_mean_speed()
-        temp2 = temp - self.coeff_calorie_2
-        return temp2 * self.weight / self.M_IN_KM * self.duration * 60
+        return ((self.coeff_calorie_1
+                * self.get_mean_speed()
+                - self.coeff_calorie_2)
+                * self.weight
+                / self.M_IN_KM
+                * self.duration * 60)
 
 
 @dataclass
@@ -75,12 +76,15 @@ class SportsWalking(Training):
 
     height: float  # Рост спортсмена
 
+    coeff_calorie_1: ClassVar[Union[int, float]] = 0.035  # Коэф-т для расчета
+    coeff_calorie_2: ClassVar[Union[int, float]] = 0.029  # Коэф-т для расчета
+
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        temp1 = (self.coeff_calorie_3 * self.weight)
-        temp = (self.get_mean_speed() ** 2) // self.height
-        temp = temp1 + temp * self.coeff_calorie_4 * self.weight
-        return temp * self.duration * 60
+        return ((self.get_mean_speed() ** 2 // self.height
+                * self.coeff_calorie_2 * self.weight
+                + self.coeff_calorie_1 * self.weight)
+                * self.duration * 60)
 
 
 @dataclass
@@ -90,34 +94,41 @@ class Swimming(Training):
     length_pool: float  # длина бассейна
     count_pool: float  # количество проплытых бассейнов
 
-    LEN_STEP = 1.38
+    LEN_STEP: ClassVar[float] = 1.38  # расстояние одного гребка
+    coeff_calorie_1: ClassVar[Union[int, float]] = 1.1  # Коэф-т для расчета
+    coeff_calorie_2: ClassVar[Union[int, float]] = 2  # Коэф-т для расчета
+
 
     def get_mean_speed(self):
         """Метод возвращает значение средней
         скорости движения во время тренировки"""
-        temp = self.length_pool * self.count_pool
-        return temp / self.M_IN_KM / self.duration
+        return (self.length_pool
+                * self.count_pool
+                / self.M_IN_KM
+                / self.duration)
 
     def get_spent_calories(self):
         """Метод возвращает число потраченных колорий"""
-        temp = (self.get_mean_speed() + self.coeff_calorie_6) * self.weight
-        return temp * self.coeff_calorie_7
+        return ((self.get_mean_speed()
+                + self.coeff_calorie_1)
+                * self.weight
+                * self.coeff_calorie_2)
 
 
-def read_package(workout_type: str, data: list) -> Training:
+def read_package(workout_type: str, data: List[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
 
     dict_class = {'SWM': (Swimming, 5),
                   'RUN': (Running, 3),
                   'WLK': (SportsWalking, 4)}
 
-    if workout_type in dict_class:  # Проверяем соответствие типа тренеровки и
-        if len(data) == dict_class[workout_type][1]:  # размер пакета данных
-            return dict_class[workout_type][0](*data)
-        else:
-            raise ValueError('Ошибка размера пакета данных')
-    else:
+    if workout_type not in dict_class:
         raise TypeError('Неизвестный тип тренировки')
+
+    if len(data) != dict_class[workout_type][1]:
+        raise ValueError('Ошибка размера пакета данных')
+
+    return dict_class[workout_type][0](*data)
 
 
 def main(training: Training) -> None:
